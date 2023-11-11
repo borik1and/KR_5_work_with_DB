20f9e02cc39083fb507e63230c579d8b0e1bdca9
Boris <borik1and@gmail.com>
2023-11-11 13:29:00 +0200

    add lines code

--- src/class_dbmanager.py
+++ src/class_dbmanager.py
@@ -1,5 +1,6 @@
 import psycopg2
 import csv
+from typing import Any
 
 
 def open_csv_file(file):
@@ -24,9 +25,8 @@ db_config = {
 
 with psycopg2.connect(**db_config) as conn:
     with conn.cursor() as cur:
-        cur.executemany("INSERT INTRO employees VALUES(%s, %s, %s, %s, %s, %s)", [i for i in employees])
-        cur.executemany("INSERT INTRO customers VALUES(%s, %s, %s)", [i for i in customers])
-        cur.executemany("INSERT INTRO orders VALUES(%s, %s, %s, %s, %s)", [i for i in orders])
+        cur.executemany("INSERT INTRO vacancyes VALUES(%s, %s, %s, %s, %s, %s)", [i for i in employees])
+        cur.executemany("INSERT INTRO employers VALUES(%s, %s, %s)", [i for i in customers])
 conn.close()
 
 
@@ -54,3 +54,78 @@ class DBManager:
     def get_vacancies_with_keyword(self):
         # получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python.
         pass
+
+
+def create_database(database_name: str, params: dict) -> None:
+    """создание базы данных и таблиц"""
+
+    conn = psycopg2.connect(dbname='postgres', **db_config)
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    cur.execute(f"DROP DATABASE {database_name}")
+    cur.execute(f"CREATE DATABASE {database_name}")
+
+    conn.close()
+
+    conn = psycopg2.connect(dbname=database_name, **params)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+                CREATE TABLE vacancy (
+                    id_vacancy INT,
+                    title_vacancy VARCHAR(100) NOT NULL,
+                    url_vacancy TEXT,
+                    selary_from INT,
+                    selary_to INT,
+                    selary_currency VARCHAR(10),
+                    employer_id INT
+                )
+            """)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+                CREATE TABLE employer (
+                    employer_id INT REFERENCES vacancy(employer_id),
+                    employer_name VARCHAR(50),
+                    employer_url TEXT
+                )
+            """)
+
+    conn.commit()
+    conn.close()
+
+
+def save_data_to_database(data: list[dict[str, Any]], database_name: str, params: dict) -> None:
+    """сохранение данных в базу данныз"""
+
+    conn = psycopg2.connect(dbname=database_name, **params)
+
+    with conn.cursor() as cur:
+        for channel in data:
+            channel_data = channel['channel']['snippet']
+            channel_stats = channel['channel']['statistics']
+            cur.execute(
+                """
+                INSERT INTO channels (title, views, subscribers, videos, channel_url)
+                VALUES (%s, %s, %s, %s, %s)
+                RETURNING channel_id
+                """,
+                (channel_data['title'], channel_stats['viewCount'], channel_stats['subscriberCount'],
+                 channel_stats['videoCount'], f"https://www.youtube.com/channel/{channel['channel']['id']}")
+            )
+            channel_id = cur.fetchone()[0]
+            videos_data = channel['videos']
+            for video in videos_data:
+                video_data = video['snippet']
+                cur.execute(
+                    """
+                    INSERT INTO videos (channel_id, title, publish_date, video_url)
+                    VALUES (%s, %s, %s, %s)
+                    """,
+                    (channel_id, video_data['title'], video_data['publishedAt'],
+                     f"https://www.youtube.com/watch?v={video['id']['videoId']}")
+                )
+
+    conn.commit()
+    conn.close()
--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
 from src.class_api import Hh_api
-from src.vacancies import Vacancy
-from src.class_save import JsonSave, add_vacancy
+# from src.class_dbmanager import create_database
+# from src.class_dbmanager import save_data_to_database
+# from src.vacancies import Vacancy
+# from src.class_save import JsonSave, add_vacancy
+# from src.config import config
 
+# params = config()
 
 keyword = input('Введите ключевые слова для фильтрации вакансий: ')
 hh = Hh_api(keyword)
 hh_vacancies = hh.format_vacancies(hh.get_vacancies())
+hh_employers = hh.format_employers(hh.get_vacancies())
 # add_vacancy()
 # Vacancy.compare_vacancies_by_salary()
 
+# save_data_to_database(data, 'vacancies', params)
+
+# create_database('vacances', {'localhost', 'postgres', '1975', '5432'})
+
 print(hh_vacancies)
+print(hh_employers)
+# print(hh.get_vacancies())
