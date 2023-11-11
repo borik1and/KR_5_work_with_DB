4b0dc655d4ef7bf8f99f591df874569e5165764f
Boris <borik1and@gmail.com>
2023-11-11 13:28:04 +0200

    changing lines in def format_employers and def format_vacancies

--- src/class_api.py
+++ src/class_api.py
@@ -23,7 +23,7 @@ class Hh_api:
         return respons.json()
 
     def format_vacancies(self, all_vacancies):
-        vacancies = {'vacancies': []}
+        vacancies = []
         for vacancy in all_vacancies['items']:
             if vacancy['salary'] is None:
                 salary = 0
@@ -35,14 +35,14 @@ class Hh_api:
                 salary = (vacancy['salary']['from'] + vacancy['salary']['to']) // 2
             new_job = {'name': vacancy['name'], 'url': vacancy['url'], 'salary': salary,
                        'experience': vacancy['experience']['name'],
+                       'emploer_name': vacancy['employer']['name'],
                        'vacancy_id': vacancy['id']}
-            vacancies['vacancies'].append(new_job)
+            vacancies.append(new_job)
         return vacancies
 
     def format_employers(self, all_vacancies):
-        employers = {'employers': []}
+        employers = []
         for employer in all_vacancies['items']:
-            new_job = {'employer_name': employer['employer']['name'],
-                       'address': employer['address'],
-                       'vacancy_id': employer['id']}
-            employers['employers'].append(new_job)
+            new_job = {'employer': employer['employer']}
+            employers.append(new_job)
+        return [i['employer'] for i in employers]
