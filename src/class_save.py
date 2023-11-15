25a2942800df5c53eda371e1ba2a0e786e4c26b1
Boris <borik1and@gmail.com>
2023-11-15 16:23:50 +0200

    add code line def save_data_to_database

--- src/class_save.py
+++ src/class_save.py
@@ -1,5 +1,16 @@
 import json
 import os
+import psycopg2
+from src.class_api import get_vacancies, format_vacancies
+from src.config import config
+from src.employers import emp
+
+params = config()
+
+hh = get_vacancies(i for i in emp)
+hh_formating = format_vacancies(hh)
+
+import psycopg2
 
 
 class JsonSave:
@@ -20,3 +31,22 @@ class JsonSave:
 def add_vacancy():
     with open('vacancies.json', 'w', encoding='utf-8') as json_file:
         json.dump(JsonSave.all_vacancies, json_file, indent=4, ensure_ascii=False)
+
+
+
+def save_data_to_database() -> None:
+    """сохранение данных в базу данныз"""
+    with psycopg2.connect(dbname='vacancies', **params) as conn:
+        with conn.cursor() as cur:
+            # Преобразование словарей в кортежи перед вставкой
+            values_for_orders = [
+                (v['employer_id'], v['vacancy_id'], v['vacancy_name'], v['url'], v['salary'], v['experience'],
+                 v['employer_name']) for v in
+                hh_formating]
+
+            # Выполнение множественной вставки с кортежами
+            cur.executemany("INSERT INTO vacancy VALUES(%s, %s, %s, %s, %s, %s, %s)", values_for_orders)
+            #
+            if isinstance(emp, dict):  # Проверяем, что emp - это словарь
+                cur.executemany("INSERT INTO employers VALUES(%s, %s)", [i for i in emp.items()])
+
