50609ff48674bf9d2c7467501c0e8f69e71da2fe
Boris <borik1and@gmail.com>
2023-11-15 16:15:16 +0200

    add code line

--- src/main.py
+++ src/main.py
@@ -1,15 +1,12 @@
-from src.class_api import get_vacancies, all_vacanciess, format_vacancies
+from src.class_api import get_vacancies, format_vacancies
 from src.class_dbmanager import create_database
 from src.config import config
 from src.employers import emp
 
 params = config()
 
+create_database('vacancies', params)
+
 hh = get_vacancies(i for i in emp)
 hh_formating = format_vacancies(hh)
-print(hh_formating)
-
-
-# create_database('vacancies', params)
-
-
+# print(hh_formating)
